605e40cc7d4a7e15eee370d5e97666d0bf21ee9f
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-07-30 21:12:03 +0530

    Update Button component to accept additional props

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Button = ({ children, textOnly, className }) => {
+const Button = ({ children, textOnly, className, ...props }) => {
   let cssClasses = textOnly ? "text-button" : "button";
   cssClasses += " " + className;
 
-  return <button className={cssClasses}>{children}</button>;
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
